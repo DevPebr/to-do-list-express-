f833e1a6778087dbc9da935b1c91b70f58923ac8
Pedro Ferreira Gomes <gomespedro82@gmail.com>
2022-11-19 19:26:27 -0300

    Adicionando a biblioteca ejs e criando uma rota com ela

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const path = require('path');
+
+
+
 const checkListRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes/index');
+
 const app  = express();
 // Data Base
 require('./config/database');
@@ -7,9 +13,13 @@ require('./config/database');
 
 app.use(express.json());
 
+
 app.use('/checklists',checkListRouter);
+app.use('/', rootRouter);
 
 
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
 
 
 app.listen(3000, () => {
